405b88b12ac8b1a7d8abe5c8ab2af688c77848f4
Nirjhar Biswas <75828293+Nirj2004@users.noreply.github.com>
2023-01-06 11:15:37 +0530

    NSIF79004999 UPLOADS.

---
+++ Currentcast/Cells/PodcastViewCell.swift
@@ -0,0 +1,37 @@
+import Cocoa
+import SDWebImage
+class PodcastCellView: NSCollectionViewItem {
+    @IBOutlet weak var poscastImage: SDAnimatedImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.wantsLayer = true
+        self.view.layer?.backgroundColor = CGColor.init(gray: 0.9, alpha: 0.2)
+        self.view.layer?.cornerRadius = 8
+        self.view.layer?.borderColor = NSColor.white.cgColor
+        self.view.layer?.borderWidth = 0.0
+        podcastImage.wantsLayer = true
+        podcastImage.layer?.cornerRadius = 8  
+    }   
+    func setHighlight(selected: Bool) {
+        NSAnimationContext.runAnimationGroup({_ in 
+            NSAnimationContext.current.duration = 0.7
+            view.layer?.borderWidth = selected ? 2.0 : 0.0
+        }, completionHandler: {
+
+        })
+    }
+    func setSearchHighlight(selected: Bool){
+        NSAnimationContext.runAnimationGroup({_ in 
+            NSAnimationContext.current.duration = 0.7
+            view.layer?.borderWidth = selected ? 4.0 : 0.0
+        }, completionHandler: {
+        })
+    }
+    func configurePodcastSearchCell(podcastCell: Parser)
+    {
+        podcastImage.sd_setImage(with: URL(string: podcastCell.imageURL), placeholderImage: NSImage(named: "placeholder"), options: .init(), completed: nil)
+    }
+    func configurePodcastAddedCell(podcastCell: String){
+        podcastImage.sd_setImage(with: URL(string: podcastCell), placeholderImage: NSImage(named: "placeholder"), options: .init(), completed: nil) 
+    }
+}
